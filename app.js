363c0b04264d14a55ffc17d8bdfca404785f35f4
rmb6283 <brako008@umn.edu>
2017-06-04 19:56:31 -0500

    RBS-NKB-0003, Support POST, add html form, and insert into database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 //path included with node by default
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //setup db
 mongoose.connect('mongodb://192.168.1.190:27024/nodekb');
@@ -27,6 +28,10 @@ var Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 //Home route
 app.get('/', function(req, res) {
     //read from Article table with empty search params
@@ -47,6 +52,26 @@ app.get('/articles/add', function(req, res) {
         title:'Add Article'
     });
 });
+
+app.post('/articles/add', function(req, res) {
+    var article = new Article();
+    //time for another dependency (body-parser)
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    //insert to db
+    article.save(function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+    console.log('Submitted');
+    return;
+})
 
 app.listen('8080', function() {
     console.log('Server started on p 8080 (as far as container knows)');
